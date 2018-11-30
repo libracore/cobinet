8c6c6e2cd15e602cbad72adbabd19de939fc02b7
lasalesi <larsericmueller@gmail.com>
2018-11-30 21:56:26 +0100

    request detects customer and provides mail field for trigger

--- cobinet/cobinet/doctype/anfrage/anfrage.py
+++ cobinet/cobinet/doctype/anfrage/anfrage.py
@@ -7,4 +7,21 @@ import frappe
 from frappe.model.document import Document
 
 class Anfrage(Document):
+    def before_save(this):
+        # make sure the owner is available as mail to field
+        if not this.email_id:
+            this.email_id = this.owner
+        # make sure customer field is set
+        if not this.kunde:
+            # find contact record
+            contact_matches = frappe.get_all("Contact", filters={'user': this.owner}, fields=['name'])
+            if contact_matches:
+                # find linked customer:
+                customer_matches = frappe.get_all("Dynamic Link", filters={'parenttype': 'Contact', 'parent': contact_matches[0]['name'], 'link_doctype': 'Customer'}, fields=['link_name'])
+                if customer_matches:
+                    customer = frappe.get_doc("Customer", customer_matches[0]['link_name'])
+                    this.kunde = customer.name
+                    this.kundenname = customer.customer_name
+        return
+        
 	pass
