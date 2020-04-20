3061c29599837cdaf5724566048bf8dbea26538a
lasalesi <larsericmueller@gmail.com>
2020-04-20 15:44:22 +0200

    improvements according to review meeting

--- cobinet/public/js/customer.js
+++ cobinet/public/js/customer.js
@@ -1,9 +1,9 @@
-/* add Preisangebot to supplier dashboard, only works for v11 and above */
+/* add GHV, activity to supplier dashboard, only works for v11 and above */
 try {
     cur_frm.dashboard.add_transactions([
         {
             'label': 'Cobinet',
-            'items': ['Geheimhaltevereinbarung']
+            'items': ['Geheimhaltevereinbarung', 'Aktivitaet']
         }
     ]);
 } catch { /* do nothing for older versions */ }
